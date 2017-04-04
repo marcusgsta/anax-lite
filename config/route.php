02c775ad7246d00bd69abe893f9c26d003b71748
marcusgsta <marcusgu@hotmail.com>
2017-04-04 19:28:42 +0200

    Second commit

---
+++ config/route.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Routes.
+ */
+ // Create the router
+$app->router = new \Anax\Route\RouterInjectable();
+
+require __DIR__ . "/route/internal.php";
+require __DIR__ . "/route/base.php";
---
+++ src/App/App.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mag\App;
+
+/**
+* An App class to wrap the resources of the framework.
+*/
+class App
+{
+
+
+}
